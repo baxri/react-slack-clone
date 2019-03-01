651adc54b30def1d4c21fd040e9821f526659f58
giorgi <giorgi.bibilashvili89@gmail.com>
2019-03-01 07:19:30 +0400

    --

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+import { BrowserRouter, Switch, Router } from "react-router-dom";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
