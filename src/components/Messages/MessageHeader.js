345f76ea2c36438564a70ba3585e69c4aa8cece7
giorgi <giorgi.bibilashvili89@gmail.com>
2019-06-08 15:25:09 +0400

    --

--- src/components/Messages/MessageHeader.js
+++ src/components/Messages/MessageHeader.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Header, Segment, Input, Icon } from "semantic-ui-react";
+import { Header, Segment, Input, Responsive } from "semantic-ui-react";
 import { HamburgerSlider } from 'react-animated-burgers'
 // import '../../../node_modules/hamburgers/dist/hamburgers.css';
 
@@ -29,16 +29,18 @@ export default class MessageHeader extends Component {
             <Segment className="messages-header">
                 <Header as="h2" floated="left" >
                     <span>
-                        <HamburgerSlider isActive={sideBarVisible} toggleButton={showSideBar} buttonWidth={30}  buttonStyle={{ outline: "none" }}/>
+                        <HamburgerSlider isActive={sideBarVisible} toggleButton={showSideBar} buttonWidth={30} buttonStyle={{ outline: "none" }} />
                         {chanel.name}
                     </span>
                     <Header.Subheader>
                         ({this.countUniqUsers(messages)}) Users, ({messages.length}) Messages
                     </Header.Subheader>
                 </Header>
-                <Header floated="right" >
-                    <Input icon="search" size="mini" loading={searchLoader} name="SearchTerm" placeholder="Search Messages" onChange={handleSearch} />
-                </Header>
+                <Responsive minWidth={500}>
+                    <Header floated="right">
+                        <Input icon="search" size="mini" loading={searchLoader} name="SearchTerm" placeholder="Search Messages" onChange={handleSearch} />
+                    </Header>
+                </Responsive>
             </Segment>
         )
     }
