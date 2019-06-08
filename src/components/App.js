af1bdbde09f36ed999ca572d4ff57a04bf29c717
giorgi <giorgi.bibilashvili89@gmail.com>
2019-06-08 14:03:31 +0400

    --

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { Grid, Image, Sidebar } from "semantic-ui-react";
+import { Grid, Image, Sidebar, Menu, Segment, Icon, Header, Button } from "semantic-ui-react";
 import './App.css';
 
 import ColorPanel from "./ColorPanel/ColorPanel";
@@ -24,72 +24,36 @@ class App extends Component {
 
         return (<div>
 
+            <Button.Group>
+                <Button disabled={visible} onClick={this.handleShowClick}>
+                    Show sidebar
+          </Button>
+                <Button disabled={!visible} onClick={this.handleHideClick}>
+                    Hide sidebar
+          </Button>
+            </Button.Group>
             <Sidebar.Pushable as={Segment}>
                 <Sidebar
-                    as={Menu}
-                    animation='overlay'
+                    animation='slide along'
                     icon='labeled'
-                    inverted
                     onHide={this.handleSidebarHide}
-                    vertical
-                    visible={visible}
-                    width='thin'
-                >
-                    <Menu.Item as='a'>
-                        <Icon name='home' />
-                        Home
-            </Menu.Item>
-                    <Menu.Item as='a'>
-                        <Icon name='gamepad' />
-                        Games
-            </Menu.Item>
-                    <Menu.Item as='a'>
-                        <Icon name='camera' />
-                        Channels
-            </Menu.Item>
+                    visible={visible}>
+                    {user && <SidePanel user={user} className="sidepanel-col" />}
                 </Sidebar>
-
                 <Sidebar.Pusher>
-                    <Segment basic>
-                        <Header as='h3'>Application Content</Header>
-                    </Segment>
+                    <Grid divided style={{ height: '100vh' }}>
+                        <Grid.Column computer={11} tablet={16} mobile={16}>
+                            {chanel &&
+                                <Messages key={chanel.id} chanel={chanel} user={user} isPrivateChanel={isPrivateChanel} showSideBar={this.handleShowClick} />}
+                        </Grid.Column>
+                        <Grid.Column computer={5} only='computer'>
+                            <MetaPanel />
+                        </Grid.Column>
+                    </Grid>
                 </Sidebar.Pusher>
             </Sidebar.Pushable>
 
-            <Grid divided style={{ height: '100vh' }}>
-                <Grid.Column computer={3} tablet={4} only='tablet computer'>
-                    {user && <SidePanel user={user} className="sidepanel-col" />}
-                </Grid.Column>
-                <Grid.Column computer={8} tablet={12} mobile={16}>
-                    {chanel &&
-                        <Messages key={chanel.id} chanel={chanel} user={user} isPrivateChanel={isPrivateChanel} />}
-                </Grid.Column>
-                <Grid.Column computer={5} only='computer'>
-                    <MetaPanel />
-                </Grid.Column>
-            </Grid>
         </div>
-            // <Grid stackable columns='equal'>
-
-            //     <Grid>
-            //         <Grid.Column only='tablet computer'>
-            //             {user && <SidePanel user={user} className="sidepanel-col"/>}
-            //         </Grid.Column>
-            //     </Grid>
-
-            //     {/* <ColorPanel /> */}
-
-            //     <Grid.Column className="messages-col">
-            //         {chanel &&
-            //         <Messages key={chanel.id} chanel={chanel} user={user} isPrivateChanel={isPrivateChanel}/>}
-            //     </Grid.Column>
-
-            //     <Grid>
-            //         <Grid.Column only='computer'>
-            //             <MetaPanel/>
-            //         </Grid.Column>
-            //     </Grid>
-            // </Grid>
         );
     }
 }
